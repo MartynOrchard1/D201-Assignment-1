b9d8d95082524ade2c76fa4dd464d88633a1c64a
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-04-02 19:35:15 +1300

    Create the linked list

--- MovieLibrary/DataStructures/Linkedlist.cs
+++ MovieLibrary/DataStructures/Linkedlist.cs
@@ -8,4 +8,27 @@ public class Node<T>
         Data = data;
         Next = null;
     }
+
+    public class LinkedList<T>
+    {
+        private Node<T> head;
+
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node<T> current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+    }
 }
