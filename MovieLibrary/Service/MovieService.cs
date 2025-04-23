ac6a595b33df62dd8b378c0689c39db4e4e861d4
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-04-23 12:43:30 +1200

    Update MovieService.cs
    #32 Added methods to add movies, retrieve all movies, search by ID, and search by title in the MovieService class.

--- MovieLibrary/Service/MovieService.cs
+++ MovieLibrary/Service/MovieService.cs
@@ -9,6 +9,23 @@ namespace MovieLibrary.Services
         private MovieLibrary.DataStructures.LinkedList<Movie> movieList = new();
         private Dictionary<string, MovieLibrary.DataStructures.Queue<string>> waitingLists = new();
 
+        public void AddMovie(Movie movie)
+        {
+            if (movieTable.ContainsKey(movie.MovieID))
+                throw new Exception("Duplicate Movie ID");
+            movieTable.Add(movie.MovieID, movie);
+            movieList.Add(movie);
+        }
+
+        public List<Movie> GetAllMovies() => movieList.ToList();
+
+        public Movie SearchByID(string id) => movieTable.Get(id);
+
+        public List<Movie> SearchByTitle(string title)
+        {
+            return movieList.ToList().Where(m => m.Title.Contains(title, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
         
     }
 }
