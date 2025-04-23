c9000aed4818c8c830f629aa5a410d2d0edf8d6e
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-04-23 12:40:14 +1200

    s

--- MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
+++ MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea7dfe5faf69d78a7ee5196b9d79c4ffbd7a3b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a7035f5621dce5d33e48103abde532f4e62f42a")]
 [assembly: System.Reflection.AssemblyProductAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MovieLibrary/views/MainWindow.xaml.cs
+++ MovieLibrary/views/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
-﻿using System.ComponentModel.Design;
-using System.Text;
+﻿using movielibrary.Models;
+using movielibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
 
 namespace MovieLibrary.Views
