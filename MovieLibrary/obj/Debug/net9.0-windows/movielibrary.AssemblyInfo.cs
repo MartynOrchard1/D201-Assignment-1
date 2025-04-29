41e03e93072eae488ae6dd4d9fb99537ced9b879
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-04-29 19:18:25 +1200

    #44 Test Successful

--- MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
+++ MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8e58631d7d02cc430c42878d09c254fa8ff268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a09e2cb587cf5c63aae1bb0a1a80f83faff76a")]
 [assembly: System.Reflection.AssemblyProductAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
