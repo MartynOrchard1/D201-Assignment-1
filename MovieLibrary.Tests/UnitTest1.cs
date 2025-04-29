276640335037394d9fc7c657e6a4f0986be2d774
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-04-29 18:34:25 +1200

    Update UnitTest1.cs
    #40 Arrange complete now onto the act/assert

--- MovieLibrary.Tests/UnitTest1.cs
+++ MovieLibrary.Tests/UnitTest1.cs
@@ -66,5 +66,42 @@ namespace MovieLibrary.Tests
             var exception = Assert.Throws<Exception>(() => _service.AddMovie(movie2));
             Assert.Equal("Duplicate Movie ID", exception.Message);
         }
+
+        [Fact]
+        public void SearchByTitle_ShouldReturnMatchingMovies()
+        {
+            // Arrange
+            var movie1 = new Movie
+            {
+                ID = "M002",
+                Title = "The Dark Knight",
+                Director = "Christopher Nolan",
+                Genre = "Action",
+                ReleaseYear = 2008
+            };
+
+            var movie2 = new Movie
+            {
+                ID = "M003",
+                Title = "Dark Waters",
+                Director = "Todd Haynes",
+                Genre = "Drama",
+                ReleaseYear = 2019
+            };
+
+            var movie3 = new Movie
+            {
+                ID = "M004",
+                Title = "Inception",
+                Director = "Christopher Nolan",
+                Genre = "Sci-Fi",
+                ReleaseYear = 2010
+            };
+
+            _service.AddMovie(movie1);
+            _service.AddMovie(movie2);
+            _service.AddMovie(movie3);
+
+        }
     }
 }
