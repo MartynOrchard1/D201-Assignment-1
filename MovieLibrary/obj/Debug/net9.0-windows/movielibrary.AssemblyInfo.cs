e8bb5b25966da27afb63c6ee828675512e067807
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-04-30 20:05:24 +1200

    Setup File Structure and add 2 screenshots for test case 1

--- MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
+++ MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b43db9f1853856f8888058617cda81525f348c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f672a39c59a8322d92ecd458d0a7c85fac80b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
