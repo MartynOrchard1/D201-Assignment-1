949077b5c69078a9ecded554280a42ed337514b1
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-05-04 13:07:48 +1200

    Ran app

--- MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
+++ MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23e873980f5ef0315a034514373b132ba238e8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc4b719e4f1a1ed1400b0c00c794c6dfad426c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
