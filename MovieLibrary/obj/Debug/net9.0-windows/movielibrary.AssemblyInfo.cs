dcefaffef377a9608f4f4c997333de87e83762b3
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-05-04 15:16:45 +1200

    #73 Generate Test Coverage Report to view
    Coverage is in the green (98% & 100%) this issue can now be closed

--- MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
+++ MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6abd742ae5dbc068796f0e1e5ec90c2496fa8410")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56c62d3b749a6cb2187f127f2e5e2096d8c3b53")]
 [assembly: System.Reflection.AssemblyProductAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
