179522fd34b8473102659ea665d63e0b4bbac695
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-05-04 14:52:05 +1200

    s

--- MovieLibrary.Tests/UnitTest1.cs
+++ MovieLibrary.Tests/UnitTest1.cs
@@ -560,7 +560,8 @@ namespace MovieLibrary.Tests
             Assert.False(queue.isEmpty());
         }
     }
-    public class newReleaseTests {
+    public class newReleaseTests 
+    {
         // These tests are for release V1.1.0
         
     }
