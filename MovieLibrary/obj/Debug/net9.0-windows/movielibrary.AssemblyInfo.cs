123b3c7c3b343f999382661c27406cd0161064c7
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-05-04 14:23:12 +1200

    #69 Add the button to the frontend

--- MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
+++ MovieLibrary/obj/Debug/net9.0-windows/movielibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2c622f07bcbb371ae5c274ac755b738854ede7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5949b91bf4972e81360b1231948855747bccadb")]
 [assembly: System.Reflection.AssemblyProductAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movielibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
